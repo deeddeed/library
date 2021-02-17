37292a17b97c76d407568ac1732003f58ff3c2b0
deeddeed <nemethdanielwebdesign@gmail.com>
2021-02-17 21:00:40 +0100

    cleaned up the code

--- myscript.js
+++ myscript.js
@@ -1,81 +1,87 @@
 const grid = document.getElementById("books");
+let myLibrary = [];
+let count = 1;
 
+//Books Object 
 function Books(title, author, pages, read) {
   this.title = title;
-  let titleField = document.createElement("BUTTON");
-  titleField.textContent = this.title;
-  titleField.style.height= "50px";
-  titleField.style.width= "250px";
-  titleField.style.backgroundColor = "#ffffff";
-  grid.append(titleField);
+  this.createButtons(this.title);
 
   this.author = author;
-  let authorField = document.createElement("BUTTON");
-  authorField.textContent = this.author;
-  authorField.style.height= "50px";
-  authorField.style.width= "250px";
-  authorField.style.backgroundColor= "#ffffff";
-  grid.append(authorField);
-
+  this.createButtons(this.author);
 
   this.pages = pages;
-  let pagesField = document.createElement("BUTTON");
-  pagesField.textContent = this.pages;
-  pagesField.style.height= "50px";
-  pagesField.style.width= "250px";
-  pagesField.style.backgroundColor="#ffffff";
-  grid.append(pagesField);
+  this.createButtons(this.pages);
 
   this.read = read;
-  let readField = document.createElement("BUTTON");
-  readField.textContent = this.read;
-  readField.style.height= "50px";
-  readField.style.width= "250px";
-  readField.style.backgroundColor="#ffffff";
-  grid.append(readField);
+  this.createButtons(this.read);
 
+  this.counterButton();
+  myLibrary[count] = [this.title, this.author, this.pages, this.read];
 
-  this.info = function () {
-    return title + author + pages + read;
-  };
+  count++;
 }
 
+//create Buttons
+Books.prototype.createButtons = function (name) {
+  let button = document.createElement("BUTTON");
+  button.textContent = name;
+  button.style.height = "50px";
+  button.style.width = "250px";
+  button.style.backgroundColor = "#ffffff";
+  grid.append(button);
+};
+
+//counter Button
+Books.prototype.counterButton = function () {
+  let countButton = document.createElement("BUTTON");
+  countButton.textContent = count;
+  grid.append(countButton);
+};
+
+//add a button that implements all the inputs from the user and create new Books object
+const addBtn = document.getElementById("add");
+addBtn.addEventListener("click", (event) => {
+  const titleField = document.getElementById("title").value;
+  const authorField = document.getElementById("author").value;
+  const pagesField = document.getElementById("pages").value;
+  const readField = document.getElementById("read").value;
+
+  const randomBook = new Books(titleField, authorField, pagesField, readField);
+  console.log(myLibrary + " Add Book");
+});
+
+//add a button Delete 
+const deleteButton = document.getElementById("delete");
+deleteButton.addEventListener("click" , (event) => {
+    myLibrary.splice((count - 1), (count - 1));
+    console.log(myLibrary + " delete Book" + (count - 1));
+})
+
 const theHobbit = new Books(
-  " The Hobbit ",
-  "J.R.R Tolkien ",
-  "295 pages ",
+  "The Hobbit",
+  "J.R.R Tolkien",
+  "295 pages",
   "not read yet"
 );
 
 const georgeOrwell = new Books(
-  " 1984 ",
-  "George Orwell ",
-  "241 pages ",
+  "1984",
+  "George Orwell",
+  "241 pages",
   "read"
 );
 
 const nickBostrom = new Books(
-  " Superintelligence",
+  "Superintelligence",
   "Nick Bostrom",
-  "311 pages ",
-  "read "
+  "311 pages",
+  "read"
 );
 
-//add a button that implements all the inputs from the user and create new Books object
-const addBtn = document.getElementById("add");
-  addBtn.addEventListener("click", (event) => {
-    const titleField = document.getElementById("title").value;
-    const authorField = document.getElementById("author").value;
-    const pagesField = document.getElementById("pages").value;
-    const readField = document.getElementById("read").value;
-
-    const randomBook = new Books(
-      titleField,
-      authorField,
-      pagesField,
-      readField
-    )
-     
-    })
-
-
+const rayKurzweil = new Books(
+  "The Singularity Is Near",
+  "Ray Kurzweil",
+  "652 pages",
+  "not yet read"
+)
