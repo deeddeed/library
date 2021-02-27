0bdfb914853fef1ceb068028062303f8486b9cde
deeddeed <nemethdanielwebdesign@gmail.com>
2021-02-27 15:13:05 +0100

    Added Butons

--- myscript.js
+++ myscript.js
@@ -2,33 +2,28 @@ const grid = document.getElementById("books");
 let myLibrary = [];
 let updatedLibrary = [];
 
-let titleInput = document.getElementById("title");
-let authorInput = document.getElementById("author");
-let pagesInput = document.getElementById("pages");
-let readInput = document.getElementById("read");
-
 //check local storage for randomBook data
+console.log("START");
 updatedLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-
 if (updatedLibrary) {
 
     myLibrary = updatedLibrary;
+    //loops thru each Item in myLibrary
     for(let i = 0; i < myLibrary.length; i++){
-  
-    console.log("Library = " + myLibrary.length + " myLibrary " + myLibrary.length + JSON.stringify(myLibrary));
-  
+      console.log("CHECK");
+    
     //creates the buttons for each book
     titleButton = document.createElement("BUTTON");
-    titleButton.textContent = JSON.stringify(myLibrary[i].title);
+    titleButton.textContent = myLibrary[i].title;
     titleButton.classList.add("titleButton");
     authorButton = document.createElement("BUTTON");
-    authorButton.textContent = JSON.stringify(myLibrary[i].author);
+    authorButton.textContent = myLibrary[i].author;
     authorButton.classList.add("authorButton");
     pagesButton = document.createElement("BUTTON");
-    pagesButton.textContent = JSON.stringify(myLibrary[i].pages);
+    pagesButton.textContent = myLibrary[i].pages;
     pagesButton.classList.add("pagesButton");
     readButton = document.createElement("BUTTON");
-    readButton.textContent = JSON.stringify(myLibrary[i].read);
+    readButton.textContent = myLibrary[i].read;
     readButton.classList.add("readButton");
     grid.append(titleButton, authorButton, pagesButton, readButton);
     }
@@ -36,7 +31,7 @@ if (updatedLibrary) {
 
 }
 
-//add a button that implements all the inputs from the user
+//add a button that implements all the inputs from the user and creates a new Book Object which gets saved in myLibrary[]
 const addBtn = document.getElementById("add");
 addBtn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -74,11 +69,26 @@ addBtn.addEventListener("click", (event) => {
   let readButton = document.createElement("BUTTON");
   readButton.textContent = randomBook.read;
   readButton.classList.add("newADD");
-
   grid.append(titleButton, authorButton, pagesButton, readButton);
   
-  console.log(myLibrary + " Add Book");
+  console.log("Add Book" + JSON.stringify(randomBook));
 
   myLibrary.push(randomBook);
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 });
+
+//Delete Button 
+const deleteBtn = document.getElementById("delete");
+deleteBtn.addEventListener("click", (event) => {
+  console.log("DELETE");
+
+  localStorage.removeItem("myLibrary");
+  location.reload();
+ 
+});
+
+//Save Button 
+function reload(){
+  location.reload();
+}
+
